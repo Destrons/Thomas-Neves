e6bba3b268648149a01b3189f398a67513dccae9
Thomas (Void) <thomas_neves@ymail.com>
2024-11-11 18:56:03 -0300

    configuração de index e atualização
    configuração de um redirecionamento ode index.html, e finalização do header da pagina.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,12 @@ import thomasImage from '../assets/thomas.png';
 
 function Header() {
     return (
-        <header className="header">
+        <header className="header, fixed-header">
             <div className="header-left">
                 <h1 className="title-txt">Thomas Neves Pereira</h1>
                 <p className="title-txt-rsm">Desenvolvedor Full Stack e especialista em infraestrutura em nuvem</p>
             </div>
-            <div className="header-right, neon-border">
+            <div className="header-right">
                 <img className="picperf" src={thomasImage} alt="Foto de Perfil" />
             </div>
         </header>
